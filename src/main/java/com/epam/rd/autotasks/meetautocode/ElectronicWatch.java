0081f9050e111c0b05bc5be8fff20ae7a849b125
Dragaar <rosivanyshyn@gmail.com>
2022-08-13 19:59:59 +0300

    111

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,6 +7,11 @@ public class ElectronicWatch {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
 
+        int hour = seconds / 3600 % 24;
+        int minutes = seconds / 60 % 60;
+        seconds = seconds / 1 % 60;
 
+        String resultTime = String.format("%01d:%02d:%02d", hour, minutes, seconds);
+        System.out.print(resultTime);
     }
 }
